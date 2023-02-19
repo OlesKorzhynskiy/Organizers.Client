135b62c525881fd8f4575cb0b5c752dee64107ef
OlesKorzhynskyy <oles.korzhynskiy@gmail.com>
2023-02-19 11:38:18 +0200

    Added home page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,20 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
 
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
     declarations: [
-        ConfirmDialogComponent
+        ConfirmDialogComponent,
+        LayoutComponent
     ],
     imports: [
         CommonModule,
         HttpClientModule,
         MatDialogModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        RouterModule,
         ToastrModule.forRoot()
+    ],
+    exports: [
+        LayoutComponent
     ]
 })
 export class CoreModule {
